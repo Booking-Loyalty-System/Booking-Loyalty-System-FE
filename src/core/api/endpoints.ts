0a0fd3c76d0ac86455e355fbff80d5521e142ac7
HideOnBushIsMe <bangnguyen290802@gmail.com>
2026-06-24 21:02:15 +0700

    chore(customer): remove unused products endpoints

--- src/core/api/endpoints.ts
+++ src/core/api/endpoints.ts
@@ -1,8 +1,4 @@
 export const ENDPOINTS = {
-    PRODUCTS: {
-        BASE: '/products',
-        DETAIL: (id: string) => `/products/${id}`,
-    },
     // Thêm các feature khác ở đây sau này...
     AUTH: {
         LOGIN: '/auth/login',
